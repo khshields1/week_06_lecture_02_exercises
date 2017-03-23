414f0533aaad68faee20488f04e916874228eff5
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-03-22 20:15:24 -0500

    week_06_lecture_02_exercises Exercise answer 2

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is week_06_lecture_02_exercises and it has exercises about objects.
---
+++ main.js
@@ -0,0 +1,13 @@
+// Answer for Exercise 2 //
+
+var randomNumber = (Math.random() * 100)
+console.log(randomNumber)
+
+var round = Math.round(randomNumber)
+console.log(round)
+
+var ceiling = Math.ceil(randomNumber)
+console.log(ceiling)
+
+var floor = Math.floor(randomNumber)
+console.log(floor)
