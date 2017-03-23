ca1f36e3c78b433b0de262b66d5f17b4e78f7233
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-03-22 20:43:06 -0500

    week_06_lecture_02_exercises Exercise answer 3

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ console.log(ceiling)
 
 var floor = Math.floor(randomNumber)
 console.log(floor)
+
+console.log('The random number is ' + randomNumber)
+console.log('The random number rounded up is ' + Math.round(randomNumber))
+console.log('The random number rounded to the next largest integer is ' + Math.ceil(randomNumber))
+console.log('The random number rounded to the next smallest integer is ' + Math.floor(randomNumber))
+
+// Answer for Exercise 3 //
+
+var randomInteger = (Math.random() * 20) - 10
+console.log(randomInteger)
